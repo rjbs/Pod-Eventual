08d43368edcb980595baa6df63682440681ab383
Ricardo SIGNES <rjbs@cpan.org>
2008-06-05 13:49:59 -0400

    docs

--- lib/Pod/Eventual.pm
+++ lib/Pod/Eventual.pm
@@ -1,7 +1,120 @@
 use strict;
 use warnings;
 package Pod::Eventual;
+# ABSTRACT: read a POD document as a series of trivial events
 use Mixin::Linewise::Readers;
+
+=head1 SYNOPSIS
+
+  package Your::Pod::Parser;
+  use base 'Pod::Eventual';
+
+  sub handle_event {
+    my ($self, $event) = @_;
+
+    print Dumper($event);
+  }
+
+=head1 DESCRIPTION
+
+POD is a pretty simple format to write, but it can be a big pain to deal with
+reading it and doing anything useful with it.  Most existing POD parsers care
+about semantics, like whether a C<=item> occurred after an C<=over> but before
+a C<back>, figuring out how to link a C<< L<> >>, and other things like that.
+
+Pod::Eventual is much less ambitious and much more stupid.  Fortunately, stupid
+is often better.  (That's what I keep telling myself, anyway.)
+
+Pod::Eventual reads line-based input and produces events describing each POD
+paragraph or directive it finds.  Once complete events are immediately passed
+to the C<handle_event> method.  This method should be implemented by
+Pod::Eventual subclasses.  If it isn't, Pod::Eventual's own C<handle_event>
+will be called, and will raise an exception.
+
+=head1 EVENTS
+
+There are three kinds of events that Pod::Eventual will produce.  All are
+represented as hash references.
+
+=head2 Command Events
+
+These events represent commands -- those things that start with an equals sign
+in the first column.  Here are some examples of POD and the event that would be
+produced.
+
+A simple header:
+
+  =head1 NAME
+
+  { type => 'command', command => 'head1', content => "NAME\n" }
+
+Notice that the content includes the trailing newline.  That's to maintain
+similarity with this possibly-surprising case:
+
+  =for HTML
+  We're actually still in the command event, here.
+
+  {
+    type    => 'command',
+    command => 'for',
+    content => "HTML\nWe're actually still in the command event, here.\n",
+  }
+
+Pod::Eventual does not care what the command is.  It doesn't keep track of what
+it's seen or whether you've used a command that isn't defined.  The only
+special case is C<=cut>, which is never more than one line.
+
+  =cut
+  We are no longer parsing POD when this line is read.
+
+  { type => 'command', command => 'cut', content => "\n" }
+
+Waiving this special case may be an option in the future.
+
+=head2 Text Events
+
+A text event is just a paragraph of text, beginning after one or more empty
+lines and running until the next empty line (or F<=cut>).  The only special
+rule is that a text event's first line must not be indented, or it will become
+a verbatim event.
+
+Text events look like this:
+
+  { type => 'text', content => "a string of text ending with a\n" }
+
+=head2 Verbatim Events
+
+Verbatim events are identical to text events, but are created when the first
+line of text begins with whitespace.  The only semantic difference is that
+verbatim events should not be subject to interpretation as POD text (for things
+like C<< L<> >> and so on).  They are often also rendered in monospace.
+
+Pod::Eventual doesn't care.
+
+=method read_handle
+
+  Pod::Eventual->read_handle($io_handle, \%arg);
+
+This method iterates through the lines of a handle, producing events and
+calling the C<handle_event> method.
+
+The only valid argument in C<%arg> (for now) is C<in_pod>, which indicates
+whether we should assume that we are parsing pod when we start parsing the
+file.  By default, this is false.
+
+This is useful to behave differently when reading a F<.pm> or F<.pod> file.
+
+=method read_file
+
+This behaves just like C<read_handle>, but expects a filename rather than a
+handle.
+
+=method read_string
+
+This behaves just like C<read_handle>, but expects a string containing POD
+rather than a handle.
+
+=cut
 
 sub read_handle {
   my ($self, $handle, $arg) = @_;
@@ -14,9 +127,9 @@ sub read_handle {
     if ($line =~ /^=cut(?:\s*)(.*?)(\n)\z/) {
       my $content = "$1$2";
       $in_pod = 0;
-      $self->record_event($current) if $current;
+      $self->handle_event($current) if $current;
       undef $current;
-      $self->record_event({
+      $self->handle_event({
         type    => 'command',
         command => 'cut',
         content => $content,
@@ -25,10 +138,12 @@ sub read_handle {
     }
 
     $in_pod = 1 if $line =~ /^=\S+/;
+
+    # consider doing $self->alien_line($line) here -- rjbs, 2008-06-05
     next LINE unless $in_pod;
 
     if ($line =~ /^$/) {
-      $self->record_event($current) if $current;
+      $self->handle_event($current) if $current;
       undef $current;
       next LINE;
     }
@@ -61,15 +176,12 @@ sub read_handle {
     $current = { type => 'text', content => $line };
   }
 
-  $self->record_event($current) if $current;
+  $self->handle_event($current) if $current;
   return;
 }
 
-sub record_event {
-  my ($self, $event) = @_;
-
-  use Data::Dumper;
-  print Dumper($event);
+sub handle_event {
+  die '...';
 }
 
 1;
--- t/basic.t
+++ t/basic.t
@@ -1,10 +1,70 @@
 use strict;
 use warnings;
 
+use Test::More tests => 1;
 use Pod::Eventual;
 
-use IO::File;
+my @events;
+{
+  package Test::Pod::Eventual;
+  our @ISA = 'Pod::Eventual';
+  sub handle_event { push @events, $_[1] }
+}
 
-my $file = IO::File->new('test.pod');
+Test::Pod::Eventual->read_file('eg/test.pod');
 
-Pod::Eventual->read_handle($file);
+my $want = [
+  {
+    type    => 'command',
+    command => 'pod',
+    content => "\nsay 3;\n",
+  },
+  {
+    type    => 'command',
+    command => 'cut',
+    content => "\n",
+  },
+  {
+    type    => 'command',
+    command => 'head1',
+    content => "NAME\n",
+  },
+  {
+    type    => 'text',
+    content => "This is a test of the NAME header.\n",
+  },
+  {
+    type    => 'command',
+    command => 'head2',
+    content => "Extended\n"
+      . "This is all part of the head2 para, whether you believe it or not.\n",
+  },
+  {
+    type    => 'text',
+    content => "Then we're in a normal text paragraph.\n",
+  },
+  {
+    type    => 'text',
+    content => "Still normal!\n",
+  },
+  {
+    type    => 'verbatim',
+    content => "  This one is verbatim.\n",
+  },
+  {
+    type    => 'text',
+    content => "Then back to normal.\n",
+  },
+  {
+    type    => 'verbatim',
+    content => "  And then verbatim\n"
+      . "Including a secondary unindented line.  Oops!  Should still work.\n",
+  },
+  {
+    type    => 'command',
+    command => 'cut',
+    content => "\n",
+  },
+];
+
+is_deeply(\@events, $want);
